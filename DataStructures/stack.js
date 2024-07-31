fc868c18a74b9d7a125c25d6a868bd4ab805338f
shaurya <shauryasingh96@gmail.com>
2024-07-31 09:56:13 +0530

    stack implementation

--- DataStructures/stack.js
+++ DataStructures/stack.js
@@ -14,6 +14,34 @@ expression conversions - infix to postfix
 call stack in JS runtime
 */
 
+class Stack {
+    constructor() {
+        this.items = []
+        this.count = 0 
+   }
+
+   push(ele){
+       this.items.push(ele)
+       this.count += 1
+   }
+   
+   pop(ele) {
+       const item = this.items[this.count]
+       this.items.pop()
+       this.count -= 1
+       return item
+   }
+
+  peek(){
+       return this.items[this.count]
+  }
+}
 
 
+const stack = new Stack()
+stack.push(1);
 
+console.log(stack);
+stack.pop()
+
+console.log(stack);
